67c81cdcd52729aeea739b47f87a5d0bb17e5647
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2023-11-13 18:10:46 +0700

    create HalamanForm

---
+++ app/src/main/java/com/adrikhamid/cobaorderviewmodel/HalamanForm.kt
@@ -0,0 +1,15 @@
+package com.adrikhamid.cobaorderviewmodel
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HalamanForm(){
+
+}
+
+@Preview (showBackground = true)
+@Composable
+fun showForm(){
+    HalamanForm()
+}
